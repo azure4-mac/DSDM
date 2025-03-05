f376273e79a4446a780aa7b822d492b473f57105
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2025-03-05 16:17:14 -0300

    Create aula.dart

---
+++ aula.dart
@@ -0,0 +1,16 @@
+void main() {
+  var name = "Flutter";
+  var image = {
+    'tags': ['flutter', 'dart', 'mobile'],
+    'url': 'https://dart.dev/'
+  };
+  print(name);
+  print(image['url']);
+
+  Map<String, dynamic> aluno = {
+    "nome": "abc",
+    "idade": 20,
+    "email": "sim@sim.sim",
+  };
+  print(aluno);
+}
